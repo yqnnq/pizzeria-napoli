65b67a53a9bf4c6643d1e858d962622b026f2e41
yqnnq <105790761+yqnnq@users.noreply.github.com>
2022-10-04 16:10:50 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // ハンバーガーメニュー
 $(function () {
-    $('.hamburger').click(function() {
+    $('.hamburger').on('click', function () {
       $(this).toggleClass('active');
 
         if ($(this).hasClass('active')) {
@@ -24,33 +24,32 @@ $(function () {
 //     });
 // });
 
-
 // アコーディオンメニュー
-$(function () {
-  $('.ac-menu').on('click', function () {
+
+// $(function () {
+//   $(".ac-menu").hover(function(){
+//     $(this).children(".ac-menu-list").stop().slideToggle();
+//   });
+// });
+
+$(function(){
+if(window.matchMedia("(max-width: 768px)").matches){
+	$('.ac-menu a').on('click',function(){
     $(this).toggleClass('active');
-    $(this).next().slideToggle();
+    $(this).next('.ac-menu-list').stop().slideToggle();
+    $('.ac-menu a').not($(this)).siblings('.ac-menu-list').slideUp();
+    $('.ac-menu a').not($(this)).removeClass('active');
   });
+}else{
+  $('.ac-menu a').on('click',function () {
+    $(this).toggleClass('active');
+    $(this).next('.ac-menu-list').stop().slideToggle();
+    $('.ac-menu a').not($(this)).siblings('.ac-menu-list').slideUp();
+    $('.ac-menu a').not($(this)).removeClass('active');
+  });
+}
 });
 
-
-// // 文字背景のアニメーション
-// function TextAnimation() {
-//   $('.text-wrapper').each(function () {
-//     var elemPos = $(this).offset().top - 30;
-//     var scroll = $(window).scrollTop();
-//     var windowHeight = $(window).height();
-//     if (scroll >= elemPos - windowHeight) {
-//       $(this).addClass('active');
-//     } else {
-//       $(this).removeClass('active');
-//     }
-//   });
-// }
-//   $(window).scroll(function (){
-//     TextAnimation();
-//   });
-// };
 
 // 文字背景のアニメーション
 $(window).on('scroll',function(){
@@ -61,7 +60,61 @@ $(window).on('scroll',function(){
     if (scroll > position - windowHeight + 100){
       $(this).addClass('active');
     } else {
-    $(this).removeClass('active');
+      $(this).removeClass('active');
+    }
+  });
+});
+
+// // 文字背景のアニメーション
+// $(window).on('scroll',function(){
+//   $('.text-wrapper').each(function () {
+//     var elemPos = $(this).offset().top - 30;
+//     var scroll = $(window).scrollTop();
+//     var windowHeight = $(window).height();
+//     if (scroll >= elemPos - windowHeight) {
+//       $(this).addClass('active');
+//     } else {
+//       $(this).removeClass('active');
+//     }
+//   });
+// });
+
+// 画像のアニメーション
+$(window).on('scroll',function(){
+  $(".fadeUpTrigger").each(function(){
+    let position = $(this).offset().top;
+    let scroll = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    if (scroll > position - windowHeight + 100){
+      $(this).addClass('fadeUp');
+    // } else {
+    // $(this).removeClass('fadeUp');
+    }
+  });
+});
+
+$(window).on('scroll',function(){
+  $(".fadeLeftTrigger").each(function(){
+    let position = $(this).offset().top;
+    let scroll = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    if (scroll > position - windowHeight + 100){
+      $(this).addClass('fadeLeft');
+    // } else {
+    // $(this).removeClass('fadeLeft');
+    }
+  });
+});
+
+$(window).on('scroll',function(){
+  $(".fadeRightTrigger").each(function(){
+    let position = $(this).offset().top;
+    let scroll = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    if (scroll > position - windowHeight + 100){
+      $(this).addClass('fadeRight');
+    // } else {
+    // $(this).removeClass('fadeRight');
     }
   });
 });
